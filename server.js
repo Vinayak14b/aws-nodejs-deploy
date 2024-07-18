bae32d19cbc9601912085264e70ae3ec686866a2
Vinayak14b <vinayakbirla20@gmail.com>
2024-07-18 12:04:59 +0530

    The second deploy with get details

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.get('/api/get',(req,res) =>{
     res.send({message:"Hello Back to node js"});
 })
 
+app.get('/api/getdetails',(req,res) =>{
+    res.send({name:"Shrivinayak Birla",
+        role :"Backend Developer"
+    });
+})
+
+
 app.listen(8000,()=>{
     console.log("listening on port 8000");
 })
